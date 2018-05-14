8138efaf90466bf05bc0f323834aec5d2a5df821
tingrun919 <344197917@qq.com>
2018-05-14 12:06:40 +0800

    修改demo

--- src/view/index/components/index-event/index-event.vue
+++ src/view/index/components/index-event/index-event.vue
@@ -3,13 +3,14 @@
 </style>
 <template>
 	<div class="event-index">
-
-
 		<van-row class="e-row">
-			<van-col span="6">礼卡区</van-col>
-			<van-col span="6">现货区</van-col>
-			<van-col span="6">礼卡提货</van-col>
-			<van-col span="6">产地溯源</van-col>
+			<van-col class="event-list" span="6">
+				<img src="../../../../assets/icon/lipin.png" width="50" height="50">
+				礼卡区
+			</van-col>
+			<van-col class="event-list" span="6"><img src="../../../../assets/icon/xianhuo.png" width="50" height="50">现货区</van-col>
+			<van-col class="event-list" span="6"><img src="../../../../assets/icon/tihuo.png" width="50" height="50">礼卡提货</van-col>
+			<van-col class="event-list" span="6"><img src="../../../../assets/icon/candi.png" width="50" height="50">产地溯源</van-col>
 		</van-row>
 	</div>
 </template>
