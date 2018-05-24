0dd7d42a02a60567c02b525e4c13d33ab1832e30
3993010@qq.com <Aa12344321>
2018-05-24 13:46:55 +0800

    购物车个人中心路由页面重新定义

--- src/router/router.js
+++ src/router/router.js
@@ -7,7 +7,7 @@ export const loginRouter = {
       title: 'login - 登录'
   },
   component: resolve => { require(['@/view/login/login.vue'], resolve); },
-  
+
 };
 export const appRouter = {
   path: '/',
@@ -49,74 +49,47 @@ export const otherRouter = [
   {
     path: '/cartDetermine',
     name: 'cartDetermine',
-    component: Other,
-    children: [
-      { path: '', title: 'cartDetermine', name: 'cartDetermine', component: resolve => { require(['@/view/cart/components/cartDetermine.vue'], resolve); } },
-    ]
+    component:  resolve => { require(['@/view/cart/components/cartDetermine.vue'], resolve); }
 	},
 	{
     path: '/cartOut',
     name: 'cartOut',
-    component: Other,
-    children: [
-      { path: '', title: 'carryOut', name: 'carryOut', component: resolve => { require(['@/view/cart/components/cartOut.vue'], resolve); } },
-    ]
+    component: resolve => { require(['@/view/cart/components/cartOut.vue'], resolve); }
   },
   {
     path: '/eventList',
     name: 'eventList',
-    component: Other,
-    children: [
-      { path: '', title: 'eventList', name: 'eventList', component: resolve => { require(['@/view/event/eventList/eventList.vue'], resolve); } },
-    ]
+    component: resolve => { require(['@/view/event/components/eventList.vue'], resolve); }
 	},
 	{
     path: '/cartAddress',
     name: 'cartAddress',
-    component: Other,
-    children: [
-      { path: '', title: 'cartAddress', name: 'cartAddress', component: resolve => { require(['@/view/cart/components/cartAddress.vue'], resolve); } },
-    ]
+    component: resolve => { require(['@/view/cart/components/cartAddress.vue'], resolve); }
   },
   {
     path: '/cartAddressEditing',
     name: 'cartAddressEditing',
-    component: Other,
-    children: [
-      { path: '', title: 'cartAddress', name: 'cartAddress', component: resolve => { require(['@/view/cart/components/cartAddressEditing.vue'], resolve); } },
-    ]
+    component: resolve => { require(['@/view/cart/components/cartAddressEditing.vue'], resolve); }
   },
   {
     path: '/shareIt',
     name: 'shareIt',
-    component: Other,
-    children: [
-      { path: '', title: 'shareIt', name: 'cartAddress', component: resolve => { require(['@/view/cart/components/shareIt.vue'], resolve); } },
-    ]
+    component: resolve => { require(['@/view/cart/components/shareIt.vue'], resolve); }
   },
   {
     path: '/shareItOut',
     name: 'shareItOut',
-    component: Other,
-    children: [
-      { path: '', title: 'shareItOut', name: 'cartAddress', component: resolve => { require(['@/view/cart/components/shareItOut.vue'], resolve); } },
-    ]
+    component:  resolve => { require(['@/view/cart/components/shareItOut.vue'], resolve); }
   },
   {
     path: '/coupon',
     name: 'coupon',
-    component: Other,
-    children: [
-      { path: '', title: 'coupon', name: 'cartAddress', component: resolve => { require(['@/view/profile/components/coupon.vue'], resolve); } },
-    ]
+    component: resolve => { require(['@/view/profile/components/coupon.vue'], resolve); }
   },
   {
     path: '/Orders',
     name: 'Orders',
-    component: Other,
-    children: [
-      { path: '', title: 'Orders', name: 'cartAddress', component: resolve => { require(['@/view/profile/components/Orders.vue'], resolve); } },
-    ]
+    component: resolve => { require(['@/view/profile/components/Orders.vue'], resolve); }
   }
 ]
 export const routers = [
