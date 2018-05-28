850274dff3e35ba4b8849b0ce184a35091d3fc8d
3993010@qq.com <Aa12344321>
2018-05-28 11:17:11 +0800

    拼团首页，已及支付成功

---
+++ src/view/teamwork/teamwork.vue
@@ -0,0 +1,56 @@
+<style lang="less" scoped>
+ @import "./teamwork.less";
+</style>
+<template>
+  <div>
+    <van-nav-bar title="我的商品" class="evetn-bar">
+      <van-icon name="arrow-left" slot="left"  class="evetn-icon" />
+    </van-nav-bar>
+    <div class="teamwork">
+      <div class="teamwork-content">
+        <div class="teamwork-img">
+          <img src="../../assets/img/CollageBg.png" alt="">
+        </div>
+        <div class="teamwork-info">
+          <p class="teamwork-info-title">
+            阳澄湖大闸蟹8对装诸如此类标题
+          </p>
+          <p class="teamwork-info-bottom">
+            ￥199.00<span>单买价￥299.00</span> <button>去开团</button>
+          </p>
+        </div>
+      </div>
+      <div class="teamwork-content">
+        <div class="teamwork-img">
+          <img src="../../assets/img/CollageBg.png" alt="">
+        </div>
+        <div class="teamwork-info">
+          <p class="teamwork-info-title">
+            阳澄湖大闸蟹8对装诸如此类标题
+          </p>
+          <p class="teamwork-info-bottom">
+            ￥199.00<span>单买价￥299.00</span> <button>去开团</button>
+          </p>
+        </div>
+      </div>
+      <div class="teamwork-content">
+        <div class="teamwork-img">
+          <img src="../../assets/img/CollageBg.png" alt="">
+        </div>
+        <div class="teamwork-info">
+          <p class="teamwork-info-title">
+            阳澄湖大闸蟹8对装诸如此类标题
+          </p>
+          <p class="teamwork-info-bottom">
+            ￥199.00<span>单买价￥299.00</span> <button>去开团</button>
+          </p>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+    name: "teamwork"
+}
+</script>
